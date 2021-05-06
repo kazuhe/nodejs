55b11b2d8196c50a7010f4b3d1609c90c4d62643
kazuhe <omc.ohara@gmail.com>
2021-05-07 02:27:12 +0900

    add: About core modules and JSON in CommonJS

--- commonjs/main.js
+++ commonjs/main.js
@@ -3,6 +3,18 @@
  * 'require()'の引数にディレクトリの指定が可能
  */
 const math = require('./math')
-
 console.log(math.add(1, 2))
 console.log(math.substract(1, 2))
+
+/**
+ * JSONを'require()'するとJavaScriptの
+ * オブジェクトとしてパースされた状態でロードされる
+ */
+const sampleJSON = require('./sample')
+console.log(sampleJSON)
+
+/**
+ * Node.jsのコアモジュールも'require()'を使ってロードする
+ */
+const fs = require('fs')
+console.log(fs)
