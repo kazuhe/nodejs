a4dd64aa47cc8863b8c550178a73473e31eb9874
kazuhe <omc.ohara@gmail.com>
2021-05-11 10:23:21 +0900

    add: About iterator and iterable

--- async/generator/index.js
+++ async/generator/index.js
@@ -25,18 +25,76 @@ function* generatorFunc() {
 // 'yield'までが実行され、最後まで到達するとそれ以降は
 // { value: undefined, done: true }が返される
 const generator = generatorFunc()
-generator.next()
+console.log(generator.next())
 // Start generator
 // yield 1
+// { value: 1, done: false }
 
-generator.next()
+console.log(generator.next())
 // yield 2
+// { value: 2, done: false }
 
-generator.next()
+console.log(generator.next())
 // yield 3
+// { value: 3, done: false }
 
-generator.next()
+console.log(generator.next())
 // End generator
+// { value: 'generator value', done: true }
 
 console.log(generator.next())
 // { value: undefined, done: true }
+
+console.log('========')
+/**
+ * イテレータとイテラブル
+ */
+// next()メソッド（valueとdoneの2つのプロパティを含むオブジェクトを返す）
+// はイテレータプロトコルの仕様に準拠している
+// イテレータプロトコルは値の配列を生成する為の標準的な方法を定義したもの
+// イテレータプロトコルを実装したオブジェクトのことをイテレータという
+
+// ジェネレータはイテラブルプロトコルという使用を満たしたイテラブルでもある
+// これは[Symbol.iterator]()メソッドによってイテレータが返される
+const generator2 = generatorFunc()
+const iterator = generator2[Symbol.iterator]()
+
+console.log(iterator.next())
+// Start generator
+// yield 1
+// { value: 1, done: false }
+
+console.log(iterator.next())
+// yield 2
+// { value: 2, done: false }
+
+console.log(iterator.next())
+// yield 3
+// { value: 3, done: false }
+
+console.log(iterator.next())
+// End generator
+// { value: 'generator value', done: true }
+
+console.log(iterator.next())
+// { value: undefined, done: true }
+
+// ジェネレータのイテラブルプロトコルから返されるイテレータは
+// ジェネレータそのものと同一
+console.log(iterator === generator2)
+// true
+
+// イテラブルはその名の通り反復可能オブジェクト
+const generator3 = generatorFunc()
+// for...ofで値を取り出す
+for (const v of generator3) {
+  console.log('for...of', v)
+}
+// Start generator
+// yield 1
+// for...of 1
+// yield 2
+// for...of 2
+// yield 3
+// for...of 3
+// End generator
