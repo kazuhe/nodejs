baddf4b8cbe22e79b58851c60b2251103f875c6f
kazuhe <omc.ohara@gmail.com>
2021-05-30 16:12:14 +0900

    add: Multi-process with cluster module

---
+++ multi/webapp/index.js
@@ -0,0 +1,30 @@
+/**
+ * webアプリケーションのマルチプロセス化
+ *
+ * ./multi_process.jsのclusterモジュールによって以下の処理をマルチプロセスで実行させる
+ */
+
+'use strict'
+
+const http = require('http')
+const fibonacci = require('./fibonacci')
+
+// サーバーオブジェクトの生成とリクエストハンドラの設定
+http
+  .createServer((req, res) => {
+    // localhost:3000/10 へのリクエストでreq.urlは'/10'になる
+    // そこから1文字を取り除いてnを取得
+    const n = Number(req.url.substr(1))
+    if (Number.isNaN(n)) {
+      // Number.isNaN()で数値化どうか判定し、数値でなかった場合は無視
+      return res.end()
+    }
+    const result = fibonacci(n)
+    // res.end()で計算結果をレスポンスとして返す
+    res.end(result.toString())
+  })
+  .listen(3000) // 3000ポートでリクエストを待機
+
+// 負荷テスト
+// node ./multi/webapp/
+// npx loadtest -c 100 -t 10 http://localhost:3000/30
---
+++ multi/webapp/multi_pocess.js
@@ -0,0 +1,33 @@
+/**
+ * マルチプロセスで動かすための処理
+ *
+ * multi/webapp/index/をマルチプロセスで実行する
+ */
+
+'use strict'
+
+const { fork, setupMaster } = require('cluster')
+
+console.log('メインプロセス', process.pid)
+
+// サブプロセスが実行するファイルの指定
+setupMaster({ exec: `${__dirname}/index` })
+
+// CPUコアの数だけプロセスをフォーク
+const cpuCount = require('os').cpus().length
+for (let i = 0; i < cpuCount; i++) {
+  // フォークしたプロセスの情報などを保持するcluster.Workerインスタンスを返す
+  const sub = fork()
+  console.log('サブプロセス', sub.process.pid)
+}
+
+// clusterモジュールによるマルチプロセス化は上記のように
+// setupMaster()メソッドを使ってサブブロセスを実行するファイルを指定して、
+// CPUコアの数だけfork()を実行してプロセスをフォークする
+
+// 並列度の上限はコアの数までなのでコア数以上にプロセスをフォークすることは意味がない
+// 逆にプロセス間のコンテキストスイッチによるコストが発生する
+
+// 負荷テスト
+// node ./multi/webapp/multi_pocess/
+// npx loadtest -c 100 -t 10 http://localhost:3000/30
